0d41030c308de568f4b62f8a7f63586c046b177a
Nejc Drobnič <ethoarx@gmail.com>
2021-12-17 23:12:30 +0100

    Update top.sv

--- tutorials/projectf/arty/1/smooth_operator/top.sv
+++ tutorials/projectf/arty/1/smooth_operator/top.sv
@@ -1,3 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.12.2021 19:51:08
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 module top (
     input wire logic [3:0] sw,
     input wire logic [3:0] btn,
